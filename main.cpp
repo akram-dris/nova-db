ca4bff2e36da18988d086fa8316033d93e8d5c9b
akram <akram7.dris7@gmail.com>
2025-10-20 04:10:36 +0100

    feat(cli): Implement core REPL

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+
+void print_prompt() {
+    std::cout << "nova> ";
+}
 
 int main() {
-    std::cout << "Welcome to NovaDB!" << std::endl;
+    std::string input_line;
+
+    while (true) {
+        print_prompt();
+        if (!std::getline(std::cin, input_line)) {
+            break; // End of input
+        }
+
+        if (input_line == ".exit") {
+            std::cout << "Goodbye from NovaDB." << std::endl;
+            break;
+        }
+
+        std::cout << "You entered: " << input_line << std::endl;
+    }
+
     return 0;
 }
